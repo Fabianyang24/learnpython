c59799b46074e0fd9069715c0249148a84d97aeb
Fabianyang24 <luckyboxbox36@gmail.com>
2023-11-11 21:09:06 +0800

    added sum & sum_list function

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def sum_list(list1):
+  total = 0
+  for a in list1:
+    total += a 
+  return total 
+
+def sum(num1,num2):
+  return num1 + num2
+
+def one():
+  print("1")
+
 if __name__ == "__main__":
   print("My first python")
-
+  one()
+  print(sum(5,6))
+  total = sum(1,2)
+  print(total) 
+  list1 = [1,2,3,4,5]
+  print(sum_list(list1))
+    
